2848337a5b249c5e3fe1bab57a31f7c27d00c63f
VladKoch_ <vlad2002kochurko@gmail.com>
2020-09-30 18:56:03 +0300

    init: start task *toReadable*

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+  let numArray = toString(number).split('');
+  let name = {
+      1 : 'one',
+      2 : 'two',
+      3 : 'tree',
+      4 : 'four',
+      5 : 'five',
+      6 : 'seven',
+      8 : 'eight',
+      9 : 'nine',
+      10 : 'hundred'
+  }
+
+  if (numArray.length == 1) {
+      return name[number];
+  }
 }
+
